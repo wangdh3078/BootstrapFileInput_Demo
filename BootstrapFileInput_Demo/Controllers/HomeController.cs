7b00a58f18f38c16b4eef7232c48ff5cae28324b
wangdh3078 <wangdh3078@126.com>
2016-12-07 16:29:59 +0800

    添加项目文件。

---
+++ BootstrapFileInput_Demo/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrapFileInput_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Documentation()
+        {
+            return View();
+        }
+
+        public JsonResult Upload()
+        {
+            HttpPostedFileBase fileData = Request.Files[0];
+            if (fileData != null)
+            {
+                try
+                {
+                    // 文件上传后的保存路径
+                    string filePath = Server.MapPath("~/Uploads/");
+                    if (!Directory.Exists(filePath))
+                    {
+                        Directory.CreateDirectory(filePath);
+                    }
+                    string fileName = Path.GetFileName(fileData.FileName);// 原始文件名称
+                    string fileExtension = Path.GetExtension(fileName); // 文件扩展名
+                    string saveName = Guid.NewGuid().ToString() + fileExtension; // 保存文件名称
+
+                    fileData.SaveAs(filePath + saveName);
+
+                    return Json(new { Success = true, FileName = fileName, SaveName = "/Uploads/" + saveName, Url = "/Uploads/" + saveName });
+                }
+                catch (Exception ex)
+                {
+                    return Json(new { Success = false, Message = ex.Message }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            else
+            {
+
+                return Json(new { Success = false, Message = "请选择要上传的文件！" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+    }
+}
---
+++ BootstrapFileInput_Demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BootstrapFileInput_Demo.MvcApplication" Language="C#" %>
